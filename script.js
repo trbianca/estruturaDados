b0802a82b8e77ebefbccdc6b61dbdac4ae6f6ad8
trbianca <trbianca@outlook.com>
2022-01-13 14:44:42 -0300

    Desafio 1 Estrutura de Dados - MOD2 - Bianca Trindade T5.1

---
+++ script.js
@@ -0,0 +1,31 @@
+// Crie uma variável que receba um nome;
+
+let nome = "Bianca";
+console.log(nome);
+
+// Crie uma variável que receba um número;
+
+let numero = 15;
+console.log(numero);
+
+// Crie uma variável com o ano atual e subtraia pelo número que você guardou;
+
+let anoAtual = 2022;
+let calculo = anoAtual-numero;
+console.log(calculo);
+
+// Agora mostre no console o resultado da soma das duas variáveis guardadas anteriormente;
+let soma = numero+calculo;
+console.log(soma);
+
+//Mostre no console a mensagem "Estou mexendo no console.log";
+
+console.log("Estou mexendo no console.log")
+
+// Crie uma variável chamada “quartaFeira” e atribua à ela o valor booleano que representa verdadeiro ou falso e mostre a saída no console;
+let quartaFeira = true;
+console.log(quartaFeira);
+
+// Exiba no console o tipo de dado da váriavel quartaFeira;
+
+console.log(typeof quartaFeira);
